fb6987effb6e51c93273a2c943398bfb4fc3ee43
Saepudin <saepudinjeh@gmail.com>
2021-11-27 20:37:37 +0700

    fix build:

--- src/components/Act-1/Intro.vue
+++ src/components/Act-1/Intro.vue
@@ -9,7 +9,7 @@
         <div class="flex flex-col justify-center">
           <div class="mx-auto relative w-full">
             <kinesis-element :strength="20" type="depth_inv" :originY=100 axis="x">
-              <img src="caracter/0.jpg" alt="1" class="w-48 h-48 md:w-96 md:h-96 object-cover bg-no-repeat bg-center bg-contain rounded-full shadow border-2 border-gray-600">
+              <img src="/caracter/intro_1.jpg" alt="1" class="w-48 h-48 md:w-96 md:h-96 object-cover bg-no-repeat bg-center bg-contain rounded-full shadow border-2 border-gray-600">
             </kinesis-element>
           </div>
         </div>
@@ -18,11 +18,11 @@
   </kinesis-container>
 
   <div data-aos="fade-down-right" class="absolute left-24 top-40 md:left-96 md:top-24">
-    <img src="chat/chat2.svg" alt="1" class="w-36 md:w-52">
+    <img src="/chat/chat2.svg" alt="1" class="w-36 md:w-52">
   </div>
 
   <div data-aos="zoom-in-left" class="absolute right-24 bottom-40 md:right-96 md:bottom-24">
-    <img src="chat/chat3.svg" alt="1" class="w-32 md:w-52">
+    <img src="/chat/chat3.svg" alt="1" class="w-32 md:w-52">
   </div>
 </div>
 </template>
