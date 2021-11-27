14dd3be66ef1e7c3e2197c7a79b244cdfd8f16ce
Saepudin <saepudinjeh@gmail.com>
2021-11-27 20:09:36 +0700

    navigation for mobile small

--- src/components/ChapterNavigation.vue
+++ src/components/ChapterNavigation.vue
@@ -10,11 +10,11 @@
 </script>
 
 <template>
-  <section class="z-40 fixed w-16 py-12 rounded-tl-3xl rounded-bl-3xl top-1/4 right-0 object-cover filter drop-shadow-lg" style="background-image: url(bg/bg2.jpg)">
+  <section class="z-40 fixed w-8 md:w-16 py-4 md:py-12 rounded-tl-3xl rounded-bl-3xl top-14 md:top-1/4 right-0 object-cover filter drop-shadow-lg" style="background-image: url(bg/bg2.jpg)">
     <div class="flex items-center my-auto">
-      <div class="flex flex-col gap-y-10 m-auto">
+      <div class="flex flex-col gap-y-4 md:gap-y-10 m-auto">
         <a v-for="navigation in navigations" :key="navigation" :href="navigation.link" class="">
-          <button class="rounded-full border-2 w-7 h-7 border-black font-bold hover:text-white hover:border-white" name="button chapter 1">
+          <button class="rounded-full border md:border-2 w-6 h-6 md:w-7 md:h-7 border-black font-bold hover:text-white hover:border-white text-sm md:text-base" name="button chapter 1">
             {{ navigation.nav }}
           </button>
         </a>
