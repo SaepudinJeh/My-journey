bc838d9c1d920eebeb6d3aa29f9163733575fb67
Saepudin <saepudinjeh@gmail.com>
2021-11-27 21:45:42 +0700

    check tailwind 4

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
 import AOS from 'aos';
 
-import './assets/tailwind.css'
-import 'aos/dist/aos.css';
+import './assets/tailwind.css';
+// import 'aos/dist/aos.css';
 import router from './router';
 import App from './App.vue';
 
