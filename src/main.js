fb82de4888d0002ad17bd0d7a35506aaadd66315
Saepudin <saepudinjeh@gmail.com>
2021-11-27 21:36:48 +0700

    fix tailwind css

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 import { createApp } from 'vue'
-// import AOS from 'aos';
+import AOS from 'aos';
 
-import '../src/assets/tailwind.css'
-// import 'aos/dist/aos.css';
+import './assets/tailwind.css'
+import 'aos/dist/aos.css';
 import router from './router';
 import App from './App.vue';
 
 const app = createApp(App);
 
 app.use(router)
-// app.use(AOS.init({
-//   once: true
-// }));
+app.use(AOS.init({
+  once: true
+}));
 app.mount('#app')
